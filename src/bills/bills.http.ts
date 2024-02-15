33a5e0fbd828395edaa1b28e90e3b05b2b08ab30
esteban-gs <26440947+esteban-gs@users.noreply.github.com>
2024-02-14 23:33:14 -0800

    Create setiment http

--- src/bills/bills.http.ts
+++ src/bills/bills.http.ts
@@ -1,5 +1,6 @@
 import { messages$ } from "../App/message.store";
 import { API_URL } from "../config";
+import { CreateSentiment } from "../sentiments/sentiments.model";
 import { Bill } from "./bills.model";
 
 export const billsApi = `${API_URL}/bills.json` as string;
@@ -20,4 +21,36 @@ export const fetchBills = async () => {
   }
 
   return parsed ?? ([]satisfies Bill[]);
+};
+
+export const createBillSentiment = async (
+  sentiment: CreateSentiment,
+  onSuccess: Function,
+  onError: Function,
+) => {
+  const headers = new Headers();
+  headers.append("Content-Type", "application/json");
+
+  const body = JSON.stringify(sentiment);
+
+  let requestOptions = {
+    method: "POST",
+    headers: headers,
+    body: body,
+  };
+
+  fetch(`${API_URL}/bills/sentiments`, requestOptions)
+    .then((response) => {
+      if (response.status === 201) {
+        onSuccess();
+      }
+    })
+    .catch(() => {
+      messages$.toast.set({
+        severity: "error",
+        summary: "Error",
+        detail: "There was an error with your request",
+      });
+      onError();
+    });
 };
