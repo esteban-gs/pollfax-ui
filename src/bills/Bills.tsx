31f3935c787f66f320e52478eadf39b6c7934053
esteban-gs <26440947+esteban-gs@users.noreply.github.com>
2024-02-14 23:32:34 -0800

    Like Dislike functionality

--- src/bills/Bills.tsx
+++ src/bills/Bills.tsx
@@ -1,24 +1,19 @@
 import { useEffect } from "react";
 import BillCard from "../components/BillCard";
 import { loadBills, state$ } from "./bills.store";
-import { Memo, Show, observer } from "@legendapp/state/react";
+import { observer } from "@legendapp/state/react";
 
 const Bills = () => {
   useEffect(() => {
     loadBills.fire();
   }, []);
 
-  const yes = (billNumber: string) => console.log(billNumber);
-  const no = (billNumber: string) => console.log(billNumber);
-
   return (
     <div>
       {state$.bills.get().length > 0 &&
         state$.bills
           .get()
-          .map((bill: any) => (
-            <BillCard key={bill.number} {...bill} yes={yes} no={no} />
-          ))}
+          .map((bill: any) => <BillCard key={bill.number} {...bill} />)}
     </div>
   );
 };
--- src/components/BillCard.tsx
+++ src/components/BillCard.tsx
@@ -1,39 +1,88 @@
 import { Bill } from "../bills/bills.model";
 import { Card } from "primereact/card";
 import { Button } from "primereact/button";
+import { createBillSentiment } from "../bills/bills.http";
+import { CreateSentiment } from "../sentiments/sentiments.model";
+import { useState } from "react";
+import { ProgressSpinner } from "primereact/progressspinner";
 
 interface Props extends Bill {
   yes: (billNumber: string) => void;
   no: (billNumber: string) => void;
 }
 
+const Loading = () => {
+  return (
+    <div className="flex align-items-center justify-content-start">
+      <div>
+        <ProgressSpinner style={{ width: "35px", height: "35px" }} />
+      </div>
+    </div>
+  );
+};
 const BillCard = (props: Props) => {
   const {
     number,
-    originChamber,
     title,
     type,
     updateDateIncludingTextString,
     latestActionText,
-    url,
-    yes,
-    no,
+    id,
   } = props;
 
-  const footer = (billNumber: string) => (
+  const [disabled, setDisabled] = useState(false);
+  const [likeLoading, setLikeLoading] = useState(false);
+  const [disLikeLoading, setDislikeLoading] = useState(false);
+
+  const like = () => {
+    setLikeLoading(true);
+    createBillSentiment(
+      { billId: id, sentiment: "like" } satisfies CreateSentiment,
+      () => {
+        setDisabled(true);
+        setLikeLoading(false);
+      },
+      () => console.log("error"),
+    );
+  };
+
+  const dislike = () => {
+    setDislikeLoading(true);
+    createBillSentiment(
+      { billId: id, sentiment: "dislike" } satisfies CreateSentiment,
+      () => {
+        setDisabled(true);
+        setDislikeLoading(false);
+      },
+      () => console.log("error"),
+    );
+  };
+
+  const footer = () => (
     <div className="flex flex-wrap justify-content-end gap-4">
-      <Button
-        label="Yes"
-        icon="pi pi-thumbs-up"
-        onClick={() => yes(billNumber)}
-        className="p-button-outlined p-button-success"
-      />
-      <Button
-        label="No"
-        icon="pi pi-thumbs-down"
-        className="p-button-outlined p-button-danger"
-        onClick={() => no(billNumber)}
-      />
+      {likeLoading ? (
+        <Loading />
+      ) : (
+        <Button
+          label="Yes"
+          icon="pi pi-thumbs-up"
+          onClick={() => like()}
+          className="p-button-outlined p-button-success"
+          disabled={disabled}
+        />
+      )}
+
+      {disLikeLoading ? (
+        <Loading />
+      ) : (
+        <Button
+          label="No"
+          icon="pi pi-thumbs-down"
+          className="p-button-outlined p-button-danger"
+          onClick={() => dislike()}
+          disabled={disabled}
+        />
+      )}
     </div>
   );
 
@@ -42,7 +91,7 @@ const BillCard = (props: Props) => {
       <Card
         title={`${type} ${number}`}
         subTitle={title}
-        footer={footer(number)}
+        footer={footer()}
         className="md:w-25rem max-w-30rem min-w-20rem"
       >
         <div className="m-0">
